4f9ce78667875a2837b5a9198dad3e6f96014751
Elektropay <richard.r@everpayinc.com>
2019-06-13 14:01:57 -0500

    Update config.php

--- platform/applications/admin/config/config.php
+++ platform/applications/admin/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://app.elektropay.com/';
+$config['base_url'] = 'https://elektropay-app.herokuapp.com/';
 //$config['base_url'] = "https://".$_SERVER['HTTP_HOST']."/elektropayinc_id/";
 //$config['base_url'] = "https://".$_SERVER['HTTP_HOST']."/elektropayinc_id/backup/";
 
